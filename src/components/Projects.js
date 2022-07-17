0dc1a6a869bfd6f40fd446a490972af0043d44d4
DionIslami1 <76430212+DionIslami1@users.noreply.github.com>
2022-07-17 20:19:19 +0200

    projects id done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,24 +1,73 @@
 import React from 'react'
+import './styles/projects.scss'
+import { RiGitRepositoryLine } from 'react-icons/ri'
 
 function Projects() {
     return (
         <div className='Projects'>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
-            <h1>Projects</h1>
+            <h2>My Projects</h2>
+            <div className='Project-boxes'>
+                <div className='box'>
+                    <div className='box-title'>
+                        <RiGitRepositoryLine />
+                        <a href='https://github.com/DionIslami1/Portfolio' className='rep-link'>Portfolio</a>
+                    </div>
+                    <p>My Portfolio</p>
+                    <div className='box-bottom'>
+                        <div className='box-circle' /><p>JavaScript</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <div className='box-title'>
+                        <RiGitRepositoryLine />
+                        <a href='https://github.com/DionIslami1/Portfolio' className='rep-link'>PDDPS</a>
+                    </div>
+                    <p>Platforma Digjitalte e te Diplomuarve sipas Programeve Studimore</p>
+                    <div className='box-bottom'>
+                        <div className='box-circle' /><p>JavaScript</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <div className='box-title'>
+                        <RiGitRepositoryLine />
+                        <a href='https://github.com/DionIslami1/Portfolio' className='rep-link'>Portfolio</a>
+                    </div>
+                    <p>My Portfolio</p>
+                    <div className='box-bottom'>
+                        <div className='box-circle' /><p>JavaScript</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <div className='box-title'>
+                        <RiGitRepositoryLine />
+                        <a href='https://github.com/DionIslami1/Portfolio' className='rep-link'>Portfolio</a>
+                    </div>
+                    <p>My Portfolio</p>
+                    <div className='box-bottom'>
+                        <div className='box-circle' /><p>JavaScript</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <div className='box-title'>
+                        <RiGitRepositoryLine />
+                        <a href='https://github.com/DionIslami1/Portfolio' className='rep-link'>Portfolio</a>
+                    </div>
+                    <p>My Portfolio</p>
+                    <div className='box-bottom'>
+                        <div className='box-circle' /><p>JavaScript</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <div className='box-title'>
+                        <RiGitRepositoryLine />
+                        <a href='https://github.com/DionIslami1/Portfolio' className='rep-link'>Portfolio</a>
+                    </div>
+                    <p>My Portfolio</p>
+                    <div className='box-bottom'>
+                        <div className='box-circle' /><p>JavaScript</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
